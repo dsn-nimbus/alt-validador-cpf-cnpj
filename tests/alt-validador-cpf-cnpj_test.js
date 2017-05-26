15669c52e34efd9985e1e5fd774defb999c1efa1
Eric Mendes Dantas <ericdantas0@gmail.com>
2017-05-26 16:59:39 -0300

    .

--- tests/alt-validador-cpf-cnpj_test.js
+++ tests/alt-validador-cpf-cnpj_test.js
@@ -24,10 +24,107 @@ describe('alt.validador-cpf-cnpj', function() {
       }
     })
   })
+
+  describe('cnpjValido', function() {
+    it('deve passar em toda listagem de cnpjs válidos e inválidos e retornar as validações corretamente', function() {
+      var _cnpjs = getCNPJs();
+
+      for (var i = 0; i < _cnpjs.length; i++) {
+        var _resultado = _validador.cnpjValido(_cnpjs[i].in)
+
+        expect(_resultado).toBe(_cnpjs[i].out)
+
+        if (_resultado !== _cnpjs[i].out) {
+          explicaErro(_cnpjs[i], _cnpjs[i].out, _resultado)
+        }
+      }
+    })
+  })
 });
 
 function explicaErro(info, outputEsperado, resultadoObtido) {
-  console.error(`Esperava que ${info.in} fosse ${outputEsperado ? 'valido' : 'inválido'}, mas o mesmo é ${resultadoObtido ? 'valido' : 'inválido'}.`)
+  console.error(`Esperava que ${info.in} (${typeof info.in}) fosse ${outputEsperado ? 'válido' : 'inválido'}, mas o mesmo é ${resultadoObtido ? 'válido' : 'inválido'}.`)
+}
+
+function getCNPJs() {
+  return [
+    {
+      in: undefined,
+      out: false
+    },
+    {
+      in: '',
+      out: false
+    },
+    {
+      in: 1,
+      out: false
+    },
+    {
+      in: "00000000000000",
+      out: false
+    },
+    {
+      in: "11111111111111",
+      out: false
+    },
+    {
+      in: "22222222222222",
+      out: false
+    },
+    {
+      in: "33333333333333",
+      out: false
+    },
+    {
+      in: "44444444444444",
+      out: false
+    },
+    {
+      in: "55555555555555",
+      out: false
+    },
+    {
+      in: "66666666666666",
+      out: false
+    },
+    {
+      in: "77777777777777",
+      out: false
+    },
+    {
+      in: "88888888888888",
+      out: false
+    },
+    {
+      in: "99999999999999",
+      out: false
+    },
+    {
+      in: '91.703.186/0001-0', // faltando dígito final (9)
+      out: false
+    },
+    {
+      in: '91.703.186/0001-09',
+      out: true
+    },
+    {
+      in: 45839835000178,
+      out: true
+    },
+    {
+      in: '91.703.186/0001-09',
+      out: true
+    },
+    {
+      in: '47842385000106',
+      out: true
+    },
+    {
+      in: '33.681.154/0001-45',
+      out: true
+    }
+  ]
 }
 
 function getCPFs() {
